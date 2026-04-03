22a9508024deec526fde75ebc690da4bce240157
edson-ricardo <nosderico@gmail.com>
2026-04-02 21:56:07 -0300

    feat: add navigation drawer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "Codarse - Página Inicial",
+};
+
 export default function PageHome() {
   return (
     <main className='bg-background'>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,46 +1,81 @@
 'use client';
+import { useEffect, useState } from "react";
 import { MdMenu, MdOutlineOpenInNew } from "react-icons/md";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
-
 export const Header = () => {
-
+  const [title, setTitle] = useState('Codarse');
+  const [drawer, setDrawer] = useState(false);
   const currentPath = usePathname();
+ 
+  useEffect(() => {
+    setTitle(document.title);
+    setDrawer(false);
+  }, [currentPath]);
 
   return (
     //nav: Deixar os itens centralizados no meio da tela
     //ul: deixar os itens um do lado do outro | gap-2 = 8px
-    <nav className="flex items-center gap-6 justify-start md:justify-center bg-primary py-2 sm:py-4 px-6">
+    <nav className='flex items-center gap-6 justify-start md:justify-center bg-primary py-2 sm:py-4 px-6'>
       
-      <button className="sm:hidden">
+      <button className='sm:hidden' onClick={() => setDrawer(true)}>
         <MdMenu size={24}/>
       </button>
 
-      <ul className="flex gap-4 items-center">
+      <ul className='flex gap-4 items-center' tabIndex={drawer ? -1 : undefined}>
         <li className="my-2">
           <Link href='/' className="border-2 rounded-md py-2 px-2 font-bold">
             CODARSE
           </Link>
         </li>
-        <li className="hidden sm:block">
+        <li className='hidden sm:block'>
           <Link href='/' data-active={currentPath === '/'} className="data-[active=true]:underline">
             Início
           </Link>
         </li>
-        <li className="hidden sm:block">
+        <li className='hidden sm:block'>
           <Link href='/cursos' data-active={currentPath === '/cursos'} className="data-[active=true]:underline">
             Cursos
           </Link>
         </li>
-        <li className="hidden sm:block">
+        <li className='hidden sm:block'>
           <Link href='https://blog.codarse.com' target='_blank' className="flex gap-1 items-center">
             Blog
             <MdOutlineOpenInNew />
           </Link>
         </li>
       </ul>
-      <h1 className="sm:hidden">Codarse - Página Inicial</h1>
+
+      <div
+       data-open={drawer}
+       tabIndex={drawer ? undefined : -1}
+       onClick={() => setDrawer(false)}
+       className='bg-linear-to-r from-background fixed top-0 left-0 bottom-0 right-0 transition-transform data-[open=false]:-translate-x-full'>
+
+        <ul className='flex gap-4 flex-col p-4 w-60 h-full bg-background' onClick={event => event.stopPropagation()}>
+          <li className=''>
+            <Link href='/' data-active={currentPath === '/'} className="data-[active=true]:underline">
+              Início
+            </Link>
+          </li>
+          <li className=''>
+            <Link href='/cursos' data-active={currentPath === '/cursos'} className="data-[active=true]:underline">
+              Cursos
+            </Link>
+          </li>
+          <li className=''>
+            <Link href='https://blog.codarse.com' target='_blank' className="flex gap-1 items-center">
+              Blog
+              <MdOutlineOpenInNew />
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <h1 className='sm:hidden'>
+        {title}
+      </h1>
     </nav>
   );
 }
